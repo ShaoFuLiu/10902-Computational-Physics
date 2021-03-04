abdddac9405b15b6a33d56c11f4ec642532c720f
ShoaFuLiu <zx22920416@gamil.com>
2021-03-05 06:49:29 +0800

    20210305 Finish HW2

--- HW1-n-site.py
+++ HW1-n-site.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from numpy import linalg as LA
 
-def Spin05(n): ## Output spin matrix
+def Spin05(n, J, h, BC): ## Output ground state eigvec/eigval and spin matrix array 
     sx = ([[0,0.5],[0.5,0]])
     sz = ([[0.5,0],[0,-0.5]])
     arr = []
@@ -32,17 +32,15 @@ def Spin05(n): ## Output spin matrix
                     z = np.kron(z,np.eye(2))
         arr.append(x)
         arr.append(z)
-    return arr
 
-def S05_EigenValue(n, J, h, BC, arr): ## Output ground state eigenvalue
-    Single = 0
-    Inter = 0
+    Single = 0 ## Single site value
+    Inter = 0  ## Interaction site value
 
     for i in range(n-1):
         I = i*2
         Inter += np.matmul(arr[I+1],arr[I+3])
         if (BC == 'PBC' and i+1 == n-1):
-            Inter += np.matmul(arr[-1],arr[1]) ## last interacion Snz*S1z
+            Inter += np.matmul(arr[-1],arr[1]) ## last interacion part (Snz*S1z) for PBC
             
     for i in range(n):
         Single += arr[i*2]
@@ -51,62 +49,43 @@ def S05_EigenValue(n, J, h, BC, arr): ## Output ground state eigenvalue
     H = -J*(Inter) - h*(Single)
     w, v = LA.eigh(H)
 
-    return w[0]
+    return w ,v ,arr
 
-def S05_EigenVector(n, J, h, BC, arr): ## Output ground state eigenvector
-    Single = 0
-    Inter = 0
-
-    for i in range(n-1):
-        I = i*2
-        Inter += np.matmul(arr[I+1],arr[I+3])
-        if (BC == 'PBC' and i+1 == n-1):
-            #print(i)
-            Inter += np.matmul(arr[-1],arr[1]) ## last interacion Snz*S1z
-            
-    for i in range(n):
-        Single += arr[i*2]
-
-    ## Hamiltonion 
-    H = -J*(Inter) - h*(Single)
-    w, v = LA.eigh(H)
-    ## Test H*EVe=EVa*EVe
-    #err = np.dot(H, v[:, 0]) - w[0] * v[:, 0]
-    return v[:, 0]
-
-def S05_Expctation_Sz(n, J, h, BC, arr): ## Output ground state Sz Expectation value 
+def S05_Expctation_Sz(n, w, v, arr): ## Output ground state Sz Expectation value 
     Exp_Sz = 0
-    EVe = S05_EigenVector(n, J, h, BC, arr)
+    EVe = v[:,0]
     TEVe = np.transpose(EVe)
-    for i in range(n):
+    for i in range(n): ## sum (VT * Si * V)
         site = i+1
-        Exp_Sz += np.matmul(np.matmul(TEVe ,arr[2*site-1]) ,EVe)
-    Exp_Sz = Exp_Sz/n
+        Exp_Sz += np.matmul(np.matmul(TEVe ,arr[2*site-1]) ,EVe) 
+    Exp_Sz = Exp_Sz/n 
 
     return Exp_Sz
 
-def S05_Expctation_Sx(n, J, h, BC, arr): ## Output ground state Sz Expectation value 
+def S05_Expctation_Sx(n, w, v, arr): ## Output ground state Sx Expectation value 
     Exp_Sx = 0
-    EVe = S05_EigenVector(n, J, h, BC, arr)
+    EVe = v[:,0]
     TEVe = np.transpose(EVe)
-    for i in range(n):
+    for i in range(n): ## VT * S * V
         site = i+1
-        Exp_Sx += np.matmul(np.matmul(TEVe ,arr[2*site-2]) ,EVe)
-    Exp_Sx = Exp_Sx/n
+        Exp_Sx += np.matmul(np.matmul(TEVe ,arr[2*site-2]) ,EVe) 
+    Exp_Sx = Exp_Sx/n  ## sum<Si> / n
 
     return Exp_Sx
 
-
+## Initial condition 
 n = 4
 J = 1
 h = 0.5
 BC = 'PBC'
-arr = Spin05(n)
-"""
-EVa = S05_EigenValue(n, 1, 0.5, 'PBC', arr)
-EVe = S05_EigenVector(n, 1, 0.5, 'PBC', arr)
-print(EVe)"""
-Exp_Sz = S05_Expctation_Sz(n, J, h, BC, arr)
+
+## Calculation
+w ,v ,arr = Spin05(n, J, h, BC)
+EVal = w[0] 
+print('Eigenvalue = ',EVal)
+EVec = v[:,0]
+print('Eigenvector = ',EVec)
+Exp_Sz = S05_Expctation_Sz(n, w, v, arr)
 print('<Sz> = ',Exp_Sz)
-Exp_Sx = S05_Expctation_Sx(n, J, h, BC, arr)
+Exp_Sx = S05_Expctation_Sx(n, w, v, arr)
 print('<Sx> = ',Exp_Sx)
