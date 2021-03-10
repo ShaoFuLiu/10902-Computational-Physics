a2696d4cdb88da0a9c79bc094dc9d5e19f077933
ShaoFuLiu <zx22920416@gmail.com>
2021-03-10 13:58:18 +0800

    20210310

--- HW_n-site.py
+++ HW_n-site.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 from numpy import linalg as LA
 
-def Spin05(n, J, h, BC): ## Output ground state eigvec/eigval and spin matrix array 
+## Output ground state eigvec/eigval and spin matrix array of Spin-1/2
+def Spin05(n, J, h, BC):  
     sx = ([[0,0.5],[0.5,0]])
     sz = ([[0.5,0],[0,-0.5]])
-    arr = []
+    arr = [] 
+
     for i in range(n):
         site = i+1
         if (site == 1):
@@ -30,6 +32,7 @@ def Spin05(n, J, h, BC): ## Output ground state eigvec/eigval and spin matrix ar
                 elif (j+3 != site):
                     x = np.kron(x,np.eye(2))
                     z = np.kron(z,np.eye(2))
+        ## arr[2*i-1] = Siz; arr[2*i-2] = Six; i=1,2,3...
         arr.append(x)
         arr.append(z)
 
@@ -40,36 +43,37 @@ def Spin05(n, J, h, BC): ## Output ground state eigvec/eigval and spin matrix ar
         I = i*2
         Inter += np.matmul(arr[I+1],arr[I+3])
         if (BC == 'PBC' and i+1 == n-1):
-            Inter += np.matmul(arr[-1],arr[1]) ## last interacion part (Snz*S1z) for PBC
+            Inter += np.matmul(arr[-1],arr[1]) ## Last interacion part (Snz*S1z) for PBC
             
     for i in range(n):
         Single += arr[i*2]
 
-    ## Hamiltonion 
-    H = -J*(Inter) - h*(Single)
-    w, v = LA.eigh(H)
+    H = -J*(Inter) - h*(Single) ## Wirte down hamiltonion
+    w, v = LA.eigh(H) ## Diagonal H and find eigenvalue/eigenvector
 
     return w ,v ,arr
 
-def S05_Expctation_Sz(n, w, v, arr): ## Output ground state Sz Expectation value 
+## Output ground state Sz Expectation value
+def S05_Expctation_Sz(n, w, v, arr):  
     Exp_Sz = 0
     EVe = v[:,0]
     TEVe = np.transpose(EVe)
-    for i in range(n): ## sum (VT * Si * V)
+    for i in range(n): ## Sum(VT * Si * V)
         site = i+1
         Exp_Sz += np.matmul(np.matmul(TEVe ,arr[2*site-1]) ,EVe) 
     Exp_Sz = Exp_Sz/n 
 
     return Exp_Sz
 
-def S05_Expctation_Sx(n, w, v, arr): ## Output ground state Sx Expectation value 
+## Output ground state Sx Expectation value
+def S05_Expctation_Sx(n, w, v, arr):  
     Exp_Sx = 0
     EVe = v[:,0]
     TEVe = np.transpose(EVe)
     for i in range(n): ## VT * S * V
         site = i+1
         Exp_Sx += np.matmul(np.matmul(TEVe ,arr[2*site-2]) ,EVe) 
-    Exp_Sx = Exp_Sx/n  ## sum<Si> / n
+    Exp_Sx = Exp_Sx/n  ## Sum<Si> / n
 
     return Exp_Sx
 
